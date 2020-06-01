3afffff593a8db0f4dfdcc67af07d0bafb277c36
GourBera <gour.bera.ds@gmail.com>
2020-06-02 02:09:04 +0530

    June-2

---
+++ plugins/operators/load_fact.py
@@ -0,0 +1,39 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+
+class LoadFactOperator(BaseOperator):
+    """
+    Operator to Loads fact table in Redshift
+    
+    :param redshift_conn_id: Redshift connection_ID
+    :param table: Target table
+    :param select_sql: SQL query
+    """
+    
+    ui_color = '#F98866'
+    
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 table="",
+                 select_sql="",
+                 *args, **kwargs):
+
+        super(LoadFactOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.select_sql = select_sql
+
+    def execute(self, context):
+
+        self.log.info("Execute LoadFactOperator")
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        self.log.info("Loading data to fact table in Redshift")
+        table_insert_sql = f"""
+            INSERT INTO {self.table}
+            {self.select_sql}
+        """
+        redshift_hook.run(table_insert_sql)
